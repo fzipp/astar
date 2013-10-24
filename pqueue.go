9d60fe7d67597bf4374079072b68245b332afafc
Frederik Zipp <fzipp@gmx.de>
2013-10-24 23:46:37 +0200

    remove unused code from priority queue implementation

--- pqueue.go
+++ pqueue.go
@@ -28,16 +28,10 @@
 
 package astar
 
-import (
-	"container/heap"
-)
-
 // An item is something we manage in a priority queue.
 type item struct {
 	value    interface{} // The value of the item; arbitrary.
 	priority float64     // The priority of the item in the queue.
-	// The index is needed by update and is maintained by the heap.Interface methods.
-	index int // The index of the item in the heap.
 }
 
 // A priorityQueue implements heap.Interface and holds items.
@@ -46,20 +40,17 @@ type priorityQueue []*item
 func (pq priorityQueue) Len() int { return len(pq) }
 
 func (pq priorityQueue) Less(i, j int) bool {
-	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	// We want Pop to give us the highest, not lowest,
+	// priority so we use greater than here.
 	return pq[i].priority > pq[j].priority
 }
 
 func (pq priorityQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
 }
 
 func (pq *priorityQueue) Push(x interface{}) {
-	n := len(*pq)
 	item := x.(*item)
-	item.index = n
 	*pq = append(*pq, item)
 }
 
@@ -67,15 +58,6 @@ func (pq *priorityQueue) Pop() interface{} {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
-	item.index = -1 // for safety
 	*pq = old[0 : n-1]
 	return item
-}
-
-// update modifies the priority and value of an item in the queue.
-func (pq *priorityQueue) update(item *item, value interface{}, priority float64) {
-	heap.Remove(pq, item.index)
-	item.value = value
-	item.priority = priority
-	heap.Push(pq, item)
 }
